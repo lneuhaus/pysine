d867096d5777247313e1301b15e2241c35679ff4
Leo <neuhaus@spectro.jussieu.fr>
2017-08-28 13:48:57 +0200

    much faster, but objects not destroyed upon exit

--- pysine/pysine.py
+++ pysine/pysine.py
@@ -2,24 +2,38 @@ import numpy as np
 from pyaudio import PyAudio
 
 BITRATE = 96000.
+PYAUDIO = PyAudio()
+
+def open_stream(bitrate=BITRATE):
+    stream = PYAUDIO.open(
+            format=PYAUDIO.get_format_from_width(1),
+            channels=1,
+            rate=int(bitrate),
+            output=True)
+    return stream
+
+def close_stream(stream):
+    stream.stop_stream()
+    stream.close()
+
+STREAM = open_stream()
+
+# to terminate stream and pyaudio object
+# close_stream(stream)
+# PYAUDIO.terminate()
+
+
 
 
 def sine(frequency=440.0, duration=1.0):
     points = int(BITRATE * duration)
     times = np.linspace(0, duration, points, endpoint=False)
-    data = np.sin(times*frequency*2*np.pi)
-    data /= np.max(np.abs(data))
-    data = (data+1.)/2.
-    data *= 255
+    data = np.array((np.sin(times*frequency*2*np.pi) + 1.0)*127.5, dtype=np.int8)
+    STREAM.write(data.tostring())
+    return
     wavedata=''
     for d in data:
          wavedata += chr(int(d))  # TODO: lookup the right c-function here, extremely inefficient!
-    p = PyAudio()
-    stream = p.open(format = p.get_format_from_width(1),
-                    channels = 1,
-                    rate = int(BITRATE),
-                    output = True)
-    stream.write(wavedata)
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
+    STREAM.write(wavedata)
+
+
--- pysine/test/test_sine.py
+++ pysine/test/test_sine.py
@@ -5,9 +5,13 @@ from time import time
 class TestSine(object):
 
     def test_sine(self):
-        for duration in [0.5, 1.0, 2.0, 10.0]:
-            t0 = time()
-            sine(duration*1000.0, duration)
-            t1 = time()
-            diff = abs(t1 - t0 -duration)
-            assert diff < 0.5
+        for duration in [0.5, 1.0, 2.0, 4.0]:
+            yield self.assert_excess_duration, duration
+
+    def assert_excess_duration(self, duration):
+        t0 = time()
+        sine(duration*1000.0, duration)
+        t1 = time()
+        diff = t1 - t0 -duration
+        assert diff >= -0.05, diff
+        assert diff <= 0.05, diff
